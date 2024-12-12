fc60d3c9084cdb8f994e295ea6e33ea66d0fe015
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2024-12-12 16:38:25 +0500

    update

--- class-code/batch-11-backend-haider-main/index.js
+++ class-code/batch-11-backend-haider-main/index.js
@@ -1,16 +1,26 @@
 import express from "express";
-import userRoutes from './routes/user.js'
+import userRoutes from "./routes/user.js";
 const app = express();
 const PORT = 4000;
-app.use(express.json()); //poori app pe laga he
+app.use(express.json());
 
+const connectttt = app.connect(
+  "mongodb+srv://uzairrehan:23Pg67O4vDU9k7di@cluster0.5n9ou.mongodb.net/"
+);
+connectttt
+  .then(() => {
+    console.log("hello ji salam o alikum");
+  })
+  .catch(() => {
+    console.log("hunmmmmm");
+  });
+
+
+  
 app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.use('/user', userRoutes)
-
+app.use("/user", userRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
-
-
