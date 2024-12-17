74d6226789cdf327ea0d76e6496c69330eb85332
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2024-12-18 00:04:55 +0500

    update

--- express/index.js
+++ express/index.js
@@ -4,17 +4,11 @@ import userModel from "./models/user.js";
 import dbConnection from "./config/db.js";
 const app = express();
 
-
-
-
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-
-
-
 
 function aboutMiddleware(req, res, next) {
   const a = 1;
@@ -23,83 +17,86 @@ function aboutMiddleware(req, res, next) {
   next();
 }
 
-
-
-
-
 app.use((req, res, next) => {
   console.log("I'm in middleWare");
   return next();
 });
 
-
-
-
-
 app.get("/", (req, res) => {
   res.render("index");
 });
-
-
-
-
 
 app.get("/contact", (req, res) => {
   res.send("contact");
 });
 
-
-
-
-
 app.get("/about", aboutMiddleware, (req, res) => {
   res.send("About");
 });
 
-
-
-
 app.get("/details", (req, res) => {
   res.send("details");
 });
-
-
-
 
 app.post("/go-data", (req, res) => {
   console.log(req.body);
   res.send("sended...");
 });
 
-
-
 app.get("/register", (req, res) => {
   res.render("register");
 });
 
-
-
-
 app.post("/register", async (req, res) => {
-  const {username , password, email} = req.body
+  const { username, password, email } = req.body;
   const newUser = await userModel.create({
-    username:username,
-    password:password,
-    email:email
-  })
+    username: username,
+    password: password,
+    email: email,
+  });
 
-  // console.log(req.body);
+  console.log(newUser);
   res.send("sended");
   // res.send("user created");
 });
 
+app.get("/get-users", (req, res) => {
+  userModel.find().then((users) => {
+    res.send(users);
+  });
+});
 
+app.get("/get-specific-user", (req, res) => {
+  userModel.find({ username: "uzair shah" }).then((user) => {
+    res.send(user);
+  });
+});
 
+app.get("/get-user", (req, res) => {
+  userModel
+    .findOne({
+      username: "uzair shah",
+    })
+    .then((user) => {
+      res.send(user);
+    });
+});
 
+app.get("/update-user", async (req, res) => {
+  await userModel.findOneAndUpdate(
+    { username: "update ho gaya " },
+    {
+      username: "update",
+    }
+  );
+  res.send("updated");
+});
 
+app.get("/delete-user", async (req, res) => {
+  await userModel.findOneAndDelete({ username: "fsdfds" });
+  res.send("deleted");
+});
 
-
-
-app.listen(3000,()=>{
-  console.log(`App is running on port ${3000}`)
+app.listen(3000, () => {
+  console.log(`App is running on port localhost:${3000}`);
 });
