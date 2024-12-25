b87a517b76475ff0fc8742abbf844b121c72478d
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2024-12-26 00:06:31 +0500

    update

--- express/config/db.js
+++ express/config/db.js
@@ -1,5 +1,5 @@
-import mongoose from "mongoose";
-const dbConnection = mongoose.connect("mongodb://0.0.0.0/men").then(() => {
-  console.log("connected");
-});
-export default dbConnection;
+// import mongoose from "mongoose";
+// mongoose.connect("mongodb://0.0.0.0:27017/men").then(() => {
+//   console.log("Connected to MongoDB");
+// });
+// export default dbConnection;
--- express/index.js
+++ express/index.js
@@ -1,13 +1,32 @@
 import express from "express";
 import morgan from "morgan";
+import mongoose from "mongoose";
 import userModel from "./models/user.js";
+import cors from "cors"
 const app = express();
 
+
+// MongoDB Connection
+mongoose.connect("mongodb://0.0.0.0:27017/men").then(() => {
+  console.log("Connected to MongoDB");
+})
+.catch((e)=>{
+  console.log(`cant connect ${e}`);
+})
+
+
+
+// Middlewares
+app.use(cors("*"))
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use((req, res, next) => {
+  console.log("I'm in middleWare");
+  next();
+});
 
 function aboutMiddleware(req, res, next) {
   const a = 1;
@@ -16,25 +35,12 @@ function aboutMiddleware(req, res, next) {
   next();
 }
 
-app.use((req, res, next) => {
-  console.log("I'm in middleWare");
-  return next();
-});
 
+
+
+// Routes
 app.get("/", (req, res) => {
   res.render("index");
-});
-
-app.get("/contact", (req, res) => {
-  res.send("contact");
-});
-
-app.get("/about", aboutMiddleware, (req, res) => {
-  res.send("About");
-});
-
-app.get("/details", (req, res) => {
-  res.send("details");
 });
 
 app.post("/go-data", (req, res) => {
@@ -42,43 +48,36 @@ app.post("/go-data", (req, res) => {
   res.send("sended...");
 });
 
+
 app.get("/register", (req, res) => {
   res.render("register");
 });
 
+
 app.post("/register", async (req, res) => {
-  const { username, password, email } = req.body;
-  const newUser = await userModel.create({
-    username: username,
-    password: password,
-    email: email,
-  });
-
-  console.log(newUser);
-  res.send("sended");
-  // res.send("user created");
-});
-
-app.get("/get-users", (req, res) => {
-  userModel.find().then((users) => {
-    res.send(users);
-  });
-});
-
-app.get("/get-specific-user", (req, res) => {
-  userModel.find({ username: "uzair shah" }).then((user) => {
-    res.send(user);
-  });
-});
-
-app.get("/get-user", (req, res) => {
-  userModel
-    .findOne({
-      username: "uzair shah",
-    })
-    .then((user) => {
-      res.send(user);
+  try {
+    const { username, password, email } = req.body;
+    const newUser = await userModel.create({
+      username,
+      password,
+      email,
     });
+    console.log(newUser);
+    res.send("User created successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error creating user");
+  }
+});
+
+app.get("/get-users", async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.send(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching users");
+  }
 });
 
 app.get("/update-user", async (req, res) => {
@@ -96,6 +95,38 @@ app.get("/delete-user", async (req, res) => {
   res.send("deleted");
 });
 
+
+app.get("/get-specific-user", (req, res) => {
+  userModel.find({ username: "uzair shah" }).then((user) => {
+    res.send(user);
+  });
+});
+
+app.get("/get-user", (req, res) => {
+  userModel
+    .findOne({
+      username: "uzair shah",
+    })
+    .then((user) => {
+      res.send(user);
+    });
+});
+
+app.get("/contact", (req, res) => {
+  res.send("contact");
+});
+
+app.get("/about", aboutMiddleware, (req, res) => {
+  res.send("About");
+});
+
+app.get("/details", (req, res) => {
+  res.send("details");
+});
+
+
+
+// Start Server
 app.listen(3000, () => {
-  console.log(`App is running on port localhost:${3000}`);
+  console.log("App is running on port localhost:3000");
 });
