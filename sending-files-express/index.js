9ba89d405310eacc43e64eeacbf16acbfbfa2382
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2025-01-12 15:00:52 +0500

    search and query params

--- sending-files-express/index.js
+++ sending-files-express/index.js
@@ -6,49 +6,44 @@ const app = express();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(3000, () => {
   console.log("running");
 });
 
+
+
+
+
+// how to send a file
 // const absolutePath = path.join(import.meta.dirname,"public")
-
 // app.use(express.static(absolutePath))
-
 // app.use(express.static("public"))
-
 // app.get("/",(req,res)=>{
-
-//     const htmlFilepath = path.join(import.meta.dirname,"public","index.html")
-//     res.sendFile(htmlFilepath)
+  //     const htmlFilepath = path.join(import.meta.dirname,"public","index.html")
+  //     res.sendFile(htmlFilepath)
 // })
 
+// params dynamic
 // app.get("/user/:id", (req, res) => {
 //   res.send(req.params.id);
 // });
 
-// app.get("/user/:id/view/:name", (req, res) => {
+// multiple dynamic params
+// // app.get("/user/:id/view/:name", (req, res) => {
 //   res.send({ name: req.params.name, id: req.params.id });
 // });
 
+
+
+// getting filenama and dirname in modular js
 // console.log("dirname==>", import.meta.dirname)
 // console.log("filename==>", import.meta.filename)
 // console.log("filename==>", import.meta.url)
 // const filename =  new URL(import.meta.filename)
+
+// query
+// app.get("/courses", (req, res) => {
+//   const searchQuery = req.query.search;
+//   const categoryQuery = req.query.category;
+//   res.send({ searchQuery, categoryQuery });
+// });
