e7c182615e56fde05f77a73ea0e837c5ccc136a4
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2025-01-16 18:47:12 +0500

    learning socket

---
+++ sockets/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import http from "http";
+import path from "path";
+import { Server } from "socket.io";
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const fileName = path.resolve(import.meta.dirname, "index.html");
+
+app.get("/", (req, res) => {
+  res.sendFile(fileName);
+});
+
+io.on("connection", (socket) => {
+  console.log(`One user is connected ${socket.id}`);
+  socket.on("chatmessage", (msg) => {
+    console.log("message: " + msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("App is running on port 3000");
+});
