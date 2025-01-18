fe7a8d2216947a99c8e28ad00d58d31309883a99
Muhammad Uzair Rehan <iamsyeduzairshah@gmail.com>
2025-01-19 01:42:23 +0500

    added controllers

--- auth-code/routes/auth-routes.js
+++ auth-code/routes/auth-routes.js
@@ -1,81 +1,19 @@
-import bcrypt from "bcrypt";
-import User from "../models/user.model.js";
-import sendVerificationEmail from "../services/mailService.js";
+import {
+  verifyEmailController,
+  loginController,
+  registerController,
+} from "../controllers/auth.js";
 import { Router } from "express";
-import jwt from "jsonwebtoken";
 
 const router = Router();
 
 // Register/Signup User
-router.post("/register", async (req, res) => {
-  const { email, username, password } = req.body;
-  console.log(email, username, password);
-
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ email, username, password: hashedPassword });
-    const newUser = await user.save();
-    res.status(201).send({
-      data: user,
-      msg: "All ok, User saved",
-      err: false,
-    });
-
-    const token = jwt.sign({ email, username }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-
-    const fullURL = `http://localhost:3000/authentication/verify-email?token=${token}`;
-
-    const info = await sendVerificationEmail(email, fullURL);
-    console.log(`Email sent ${info}`);
-  } catch (error) {
-    res.send(error);
-  }
-});
+router.post("/register", registerController);
 
 // login User
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email: email });
-    const hashPassword = await bcrypt.compare(password, user.password);
-    if (hashPassword) {
-      res.status(201).send({
-        data: user,
-        msg: "All ok, User logged in",
-        err: false,
-      });
-    } else {
-      res.status(400).send({
-        msg: "Cant Login",
-        err: true,
-      });
-    }
-  } catch (error) {
-    console.log(error);
-  }
-});
+router.post("/login", loginController);
 
 // email verification
-router.get("/verify-email", async (req, res) => {
-  try {
-    const { token } = req.query;
-    const { email } = jwt.decode(token, process.env.JWT_SECRET);
-    // console.log(`email to be verify = ${email}`);
-
-    const { username } = await User.findOneAndUpdate(
-      { email: email },
-      { verified: true }
-    );
-
-    res.send(`This User is Verified ${username}`);
-  } catch (error) {
-    res.status(400).send({
-      msg: error,
-      err: true,
-    });
-  }
-});
+router.get("/verify-email", verifyEmailController);
 
 export default router;
